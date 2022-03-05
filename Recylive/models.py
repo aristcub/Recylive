b89c070e896ad0d119eea5dd030e1266157106f5
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-03-05 11:07:06 -0500

    creacion de modelo de residente

--- Recylive/models.py
+++ Recylive/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from distutils.command import upload
+from quopri import decodestring
 
-# Create your models here.
+class Residente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apartamento = models.CharField(max_length=50)
+    cedula = models.IntegerField()
+    telefono = models.IntegerField()
+    puntos = models.IntegerField()
+    
+
+
+
