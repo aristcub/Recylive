dffdd75aa0b2ecf954bf832dcd44e4a1f953d26f
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-03-27 09:43:36 -0500

    Carpeta de hardware

--- Recylive/views.py
+++ Recylive/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    searchTerm = request.GET.get('searchResident')
-    return render (request, 'home.html',{'searchTerm':searchTerm})
+    return render (request, 'home.html',{'name':'RECYLIVE'})
 
 def about(request):
-    return HttpResponse('Welcome to About page</h1>')
-
-# Create your views here.
+    return
